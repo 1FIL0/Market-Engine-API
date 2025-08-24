bba07a5309eb18a36b8dc79ffe6cac0c05cb4f5e
1FIL0 <1fil0software@gmail.com>
2025-08-24 01:39:47 +0100

    add interval arg

--- python/sonar/src/main.py
+++ python/sonar/src/main.py
@@ -8,6 +8,8 @@ import api_bymykel
 import api_steamweb
 import ready_item_processor
 import syncer
+import time
+import logger
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("--bymykel", action="store_true")
@@ -16,25 +18,30 @@ argParser.add_argument("--steamweb-key")
 argParser.add_argument("--assets", action="store_true")
 argParser.add_argument("--process", action="store_true")
 argParser.add_argument("--sync")
-argParser.add_argument("--interval")
+argParser.add_argument("--interval-sec")
 args, unknownArgs = argParser.parse_known_args()
 
 def main():
     validator.validateFiles()
-    if args.bymykel:
-        api_bymykel.refreshBymykelItems()
-    if args.steamweb:
-        steamwebKey = ""
-        if args.steamweb_key: steamwebKey = args.steamweb_key
-        api_steamweb.refreshSteamWebApiItems(steamwebKey)
-    if args.process:
-        ready_item_processor.createReadyItems()
-    if args.assets:
-        skin_refresher.refreshSkinImagesFromReadyItems()
-    if args.sync == "server":
-        syncer.updateServer()
-    elif args.sync == "local":
-        syncer.updateLocal()
+    while (1):
+        if args.bymykel:
+            api_bymykel.refreshBymykelItems()
+        if args.steamweb:
+            steamwebKey = ""
+            if args.steamweb_key: steamwebKey = args.steamweb_key
+            api_steamweb.refreshSteamWebApiItems(steamwebKey)
+        if args.process:
+            ready_item_processor.createReadyItems()
+        if args.assets:
+            skin_refresher.refreshSkinImagesFromReadyItems()
+        if args.sync == "server":
+            syncer.updateServer()
+        elif args.sync == "local":
+            syncer.updateLocal()
+        if not args.interval_sec:
+            break
+        logger.sendMessage(f"Sleeping for {args.interval_sec} seconds")
+        time.sleep(int(args.interval_sec))
 
 if __name__ == "__main__":
 	main()
